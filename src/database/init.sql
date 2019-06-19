f5c2ff012e2681113cb74387f583f76727abbcf9
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2019-06-19 01:41:22 +0100

    feat(location): creates location service (#2)
    - creates the location service
    - modify the database init file

--- src/database/init.sql
+++ src/database/init.sql
@@ -7,21 +7,7 @@ CREATE TABLE location
 	location_name VARCHAR(50) NOT NULL UNIQUE,
 	male_population INT NOT NULL,
 	female_population TIMESTAMP NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  updatedAt TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
 	PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS sublocation CASCADE;
-CREATE TABLE sublocation
-(
-	id uuid NOT NULL DEFAULT uuid_generate_v4(),
-	location_name VARCHAR(50) NOT NULL UNIQUE,
-  parent_id VARCHAR(50) NOT NULL,
-	male_population INT NOT NULL,
-	female_population TIMESTAMP NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  updatedAt TIMESTAMP NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (parent_id) REFERENCES contact (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
